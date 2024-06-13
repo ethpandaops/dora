6dd303d1c581298c1c8c208166de6d5e83377335
pk910 <philipp@pk910.de>
2024-06-14 00:04:14 +0200

    fix `converting NULL to string is unsupported` errors due to bad data after recent slots table migration

---
+++ db/schema/pgsql/20240613235955_fix-slots-data.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+-- +goose StatementBegin
+
+UPDATE slots
+SET eth_block_extra = '0x', eth_block_extra_text = ''
+WHERE state_root IS NOT NULL AND eth_block_extra IS NULL;
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'NOT SUPPORTED';
+-- +goose StatementEnd
