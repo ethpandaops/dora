0987d2b5f0b2d020139dd0be826073c701a4b89a
Rafael Matias <rafael@skyle.net>
2024-07-11 10:20:27 +0200

    fix bug where files were not being cleaned up

--- .hack/devnet/cleanup.sh
+++ .hack/devnet/cleanup.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ENCLAVE_NAME="${ENCLAVE_NAME:-dora}"
 kurtosis enclave rm -f "$ENCLAVE_NAME"
 
 echo "Cleaning up generated files..."
-rm -f "${__dir}/generated-*.yaml"
+rm -f ${__dir}/generated-*
