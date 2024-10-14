db3fff084146f8789979829708b8ef44b428ba27
Rafael Matias <rafael@skyle.net>
2024-10-14 15:41:50 +0200

    fix modal zindex to not colide with header

--- static/js/common.js
+++ static/js/common.js
@@ -1,6 +1,11 @@
   // Fix bootstrap backdrop stacking when having multiple modals
   $(document).on('show.bs.modal', '.modal', function() {
-    const zIndex = 10 + Math.max(...Array.from(document.querySelectorAll('*')).map((el) => +el.style.zIndex));
+    const zIndex = 1000 + Math.max(...Array.from(document.querySelectorAll('*')).map((el) => +el.style.zIndex));
     $(this).css('z-index', zIndex);
     setTimeout(() => $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack'));
+  });
+
+  // Fix bootstrap scrolling stacking when having multiple modals
+  $(document).on('hidden.bs.modal', '.modal', function(){
+    $('.modal:visible').length && $(document.body).addClass('modal-open')
   });
