366885c02300fd2e6f1c2c05c9d0689d9d38b292
Rafael Matias <rafael@skyle.net>
2024-10-14 15:33:53 +0200

    fix blacked out bug after closing all modals and reopening them

--- static/js/common.js
+++ static/js/common.js
@@ -1,6 +1,6 @@
   // Fix bootstrap backdrop stacking when having multiple modals
   $(document).on('show.bs.modal', '.modal', function() {
-    const zIndex = 1040 + 10 * $('.modal:visible').length;
+    const zIndex = 10 + Math.max(...Array.from(document.querySelectorAll('*')).map((el) => +el.style.zIndex));
     $(this).css('z-index', zIndex);
     setTimeout(() => $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack'));
   });
