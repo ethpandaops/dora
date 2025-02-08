30a4d6dc118f34da1e813ef08ad1888fd44d608d
pk910 <philipp@pk910.de>
2025-02-09 00:15:04 +0100

    fix schema upgrade sql for sqlite

--- db/schema/sqlite/20250208225212_epbs-payload.sql
+++ db/schema/sqlite/20250208225212_epbs-payload.sql
@@ -1,13 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 
-ALTER TABLE "unfinalized_blocks" ADD
- "payload_ver" int NOT NULL DEFAULT 0,
- "payload_ssz" BLOB NULL;
+ALTER TABLE "unfinalized_blocks" ADD "payload_ver" int NOT NULL DEFAULT 0;
+ALTER TABLE "unfinalized_blocks" ADD "payload_ssz" BLOB NULL;
 
-ALTER TABLE "orphaned_blocks" ADD
- "payload_ver" int NOT NULL DEFAULT 0,
- "payload_ssz" BLOB NULL;
+ALTER TABLE "orphaned_blocks" ADD "payload_ver" int NOT NULL DEFAULT 0;
+ALTER TABLE "orphaned_blocks" ADD "payload_ssz" BLOB NULL;
 
 -- +goose StatementEnd
 -- +goose Down
