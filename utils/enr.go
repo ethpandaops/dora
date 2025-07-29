b8b762bf86459d4786d77fdc8691ab287cf61777
Barnabas Busa <busa.barnabas@gmail.com>
2025-07-29 11:36:27 +0200

    fmt

--- utils/enr.go
+++ utils/enr.go
@@ -41,12 +41,12 @@ func GetKeyValuesFromENRFiltered(r *enr.Record) map[string]interface{} {
 
 func getKeyValuesFromENR(r *enr.Record, filterSensitive bool) map[string]interface{} {
 	fields := make(map[string]interface{})
-	
+
 	sensitiveFields := map[string]bool{
 		"ip":  true,
 		"ip6": true,
 	}
-	
+
 	fields["seq"] = r.Seq()
 	fields["signature"] = "0x" + hex.EncodeToString(r.Signature())
 
@@ -54,11 +54,11 @@ func getKeyValuesFromENR(r *enr.Record, filterSensitive bool) map[string]interfa
 	for i := 0; i < len(kv); i += 2 {
 		key := kv[i].(string)
 		val := kv[i+1].(rlp.RawValue)
-		
+
 		if filterSensitive && sensitiveFields[key] {
 			continue
 		}
-		
+
 		formatter := attrFormatters[key]
 		if formatter == nil {
 			formatter = formatAttrRaw
