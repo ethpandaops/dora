a6d19286c91b8d62f3548ddc7600150d0f761b2a
pk910 <philipp@pk910.de>
2025-08-18 13:37:19 +0200

    remove chain forks diagram from main menu

--- handlers/pageData.go
+++ handlers/pageData.go
@@ -125,11 +125,13 @@ func createMenuItems(active string) []types.MainMenuItem {
 				Path:  "/blocks",
 				Icon:  "fa-cube",
 			},
-			{
-				Label: "Chain Forks",
-				Path:  "/chain-forks",
-				Icon:  "fa-project-diagram",
-			},
+			/*
+				{
+					Label: "Chain Forks",
+					Path:  "/chain-forks",
+					Icon:  "fa-project-diagram",
+				},
+			*/
 		},
 	})
 	if len(utils.Config.MevIndexer.Relays) > 0 {
